56bcb1a4f1ba85b7c7a454ad141a4757405f59da
abdurrohimbontro <89439439+abdurrohimbontro@users.noreply.github.com>
2021-09-15 00:16:09 +0700

    Update rhelper.py

--- userbot/modules/rhelper.py
+++ userbot/modules/rhelper.py
@@ -12,22 +12,22 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Hai {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.rhelp` Atau Bisa `.help` atau Minta Bantuan Ke:\n"
-        "\n[LANDAK🦔](t.me/teman_random)"
-        "\n\n[SUPPORT](https://t.me/geezsupportgroup)"
-        "\n\n[CHANNEL](https://t.me/ramubotinfo)")
+        "\n[JoIn🌾](t.me/roommutualann)"
+        "\n\n[SUPPORT](https://t.me/crazy_people345)"
+        "\n\n[CHANNEL](https://t.me/warga_pati)")
 
 
 @register(outgoing=True, pattern="^.rvars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/ramadhani892/RAM-UBOT/RAM-UBOT/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/abdurrohimbontro/KIM-UBOT/KIM-UBOTS/varshelper.txt)")
 
 
 CMD_HELP.update({
     "ramhelper":
     "`.helpmy`\
-\nPenjelasan: Bantuan Untuk RAM-UBOT.\
+\nPenjelasan: Bantuan Untuk KIM-UBOT.\
 \n`.rvars`\
 \nPenjelasan: Untuk Melihat Beberapa Daftar Vars."
 })

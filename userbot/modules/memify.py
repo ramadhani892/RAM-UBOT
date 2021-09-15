44bfc7b048bb2f4e7b46d09839f300ee7c78ab4c
abdurrohimbontro <89439439+abdurrohimbontro@users.noreply.github.com>
2021-09-15 15:05:26 +0700

    Update memify.py

--- userbot/modules/memify.py
+++ userbot/modules/memify.py
@@ -30,7 +30,7 @@ async def mim(event):
         return
     else:
         await event.edit(
-            "```Gua edit dulu Bentar, emmm... ```"
+            "```KIM LAGI NGEDIT... ```"
         )
         await asyncio.sleep(5)
         text = event.pattern_match.group(1)
@@ -186,7 +186,7 @@ async def mim(event):
         return
     else:
         await event.edit(
-            "```Gua Edit dulu bentar, emm....```"
+            "``` KIM LAGI NGEDIT....```"
         )
         await asyncio.sleep(5)
         text = event.pattern_match.group(1)

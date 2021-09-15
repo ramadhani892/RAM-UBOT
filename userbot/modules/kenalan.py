0145113b091a0053f3ef14fde6fd587eeeb3dd14
abdurrohimbontro <89439439+abdurrohimbontro@users.noreply.github.com>
2021-09-15 15:38:28 +0700

    Update kenalan.py

--- userbot/modules/kenalan.py
+++ userbot/modules/kenalan.py
@@ -6,12 +6,12 @@ from userbot.events import register
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("`Hai Perkenalkan Namaku Ramadhani`")
-    sleep(3)
-    await typew.edit("20 Tahun`")
+    await typew.edit("`Hai Perkenalkan Namaku KIM`")
     sleep(1)
-    await typew.edit("`Tinggal Di tangerang, Salam Kenal:)`")
-# Create by myself @localheart
+    await typew.edit("kalo umur bisa di atur lah`")
+    sleep(1)
+    await typew.edit("`Tinggal Di pati,Jawa tengah, Indonesia, Salam Kenal:)`")
+# Create by myself @warga_pati
 
 
 @register(outgoing=True, pattern='^.sayang(?: |$)(.*)')
@@ -22,8 +22,8 @@ async def typewriter(typew):
     sleep(3)
     await typew.edit("`Aku Sayang Kamu`")
     sleep(1)
-    await typew.edit("`I LOVE YOU 💞`")
-# Create by myself @localheart
+    await typew.edit("`I LOVE YOU 💝`")
+# Create by myself @warga_pati
 
 
 @register(outgoing=True, pattern='^.semangat(?: |$)(.*)')
@@ -32,7 +32,15 @@ async def typewriter(typew):
     sleep(3)
     await typew.edit("`Apapun Yang Terjadi`")
     sleep(3)
-    await typew.edit("`Tetaplah Bernapas`")
-    sleep(1)
-    await typew.edit("`Dan Selalu Bersyukur`")
-# Create by myself @localheart
+    await typew.edit("`Tetaplah semangat`")
+    sleep(3)
+    await typew.edit("`meski pada akhirnya dia bukan jodohmu`")
+    sleep(3)
+    await typew.edit("`karna ini sekenario dari tuhanmu`")
+    sleep(3)
+    await typew.edit("`yang suatu saat akan menjadi kenangan indah dalam hidupmu`")
+    sleep(3)
+    await typew.edit("`jangan menyerah Dan tetap semangat setidaknya dia membuatmu bahagia untuk hari ini`")
+    sleep(3)
+    await typew.edit("`tuhan menghancurkan rencanamu sebelum rencanamu menghancurkanmu ,Tuhan lebih tau 😊`")
+# create by myself @warga_pati
